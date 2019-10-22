650fe4b87977bc73dcaf77ba4801e311fe1e0bc4
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-22 19:31:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,7 @@
 # Your code here
+def meal_choice (veg1, veg2, protein="meat")
+    puts "What a nutritious meal!"
+    order = "A plate of #{protein} with #{veg1} and #{veg2}."
+    puts order
+    return order
+end
